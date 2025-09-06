4a1ce92b8d5234016905cdaff1c78681723318f5
Wibawa12 <wibawapemaron12345678910@gmail.com>
2025-09-06 10:12:45 +0800

    Update commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,11 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <title>Budaya Bali</title> 
-=======
     <title>Budaya Bali</title>
->>>>>>> 33b14dbefeb761949665961dad80b3f62d22e992
     <!-- TAILWIND CSS CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- CUSTOM CSS -->
